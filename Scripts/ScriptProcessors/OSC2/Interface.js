a108169578c8f398aea402e8de853a84fea06348
Channel Thirteen <mr.normall@gmail.com>
2025-06-04 00:35:49 -0400

    Add files via upload

---
+++ Scripts/ScriptProcessors/OSC2/Interface.js
@@ -0,0 +1,24 @@
+Content.makeFrontInterface(800, 800);
+
+const var presetLabel = Content.getComponent("FloatingTile3");
+presetLabel.set("text", "INIT"); // or whatever you call your init presetfunction onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
